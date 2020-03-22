3a96487f681fc1e4cf75442be01c2d978f492993
pecpaker <umehpeculiar20@gmail.com>
2020-03-22 12:22:09 +0100

    Initial commit

---
+++ app/src/main/java/com/pecpacker/africaknow_game/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pecpacker.africaknow_game
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
